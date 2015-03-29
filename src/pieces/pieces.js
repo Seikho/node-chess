85dedd0c911ff2f7105033b3f8a9003dcbcf23d5
Carl <carl@carlossus.com>
2015-03-29 16:17:39 +0800

    Adding knight and bishop to piece aggreator

--- src/pieces/pieces.js
+++ src/pieces/pieces.js
@@ -1,7 +1,11 @@
 var Pawn = require("./pawn");
 var Rook = require("./rook");
+var Knight = require("./knight");
+var Bishop = require("./bishop");
 var pieces = {
     pawn: Pawn,
+    knight: Knight,
+    bishop: Bishop,
     rook: Rook
 };
 module.exports = pieces;
--- src/pieces/pieces.ts
+++ src/pieces/pieces.ts
@@ -1,8 +1,12 @@
 import Pawn = require("./pawn");
 import Rook = require("./rook");
+import Knight = require("./knight");
+import Bishop = require("./bishop");
 export = pieces;
 
 var pieces = {
 	pawn: Pawn,
+	knight: Knight,
+	bishop: Bishop,
 	rook: Rook
 };
