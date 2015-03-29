6ad398dca275254e37c9986b832cc59fe3457158
Carl <carl@carlossus.com>
2015-03-29 16:33:24 +0800

    Added all piece factories to piece aggreator

--- src/pieces/pieces.ts
+++ src/pieces/pieces.ts
@@ -2,11 +2,15 @@ import Pawn = require("./pawn");
 import Rook = require("./rook");
 import Knight = require("./knight");
 import Bishop = require("./bishop");
+import Queen = require("./queen")
+import King = require("./king");
 export = pieces;
 
 var pieces = {
 	pawn: Pawn,
 	knight: Knight,
 	bishop: Bishop,
-	rook: Rook
+	rook: Rook,
+	queen: Queen,
+	king: King
 };
