6a3f8cdfdf540c969854225221c2798a8d9c5d6f
Carl <carl@carlossus.com>
2015-04-04 10:31:54 +0800

    Removed pos() from increment tests. Replaced with coord

--- test/increments.js
+++ test/increments.js
@@ -7,19 +7,20 @@ console.log(classicBoard.toString());
 var expect = chai.expect;
 var dir = Chess.Direction;
 describe("single movement tests", function () {
-    singleMoveTest("will move the rank up one square", pos(1, 1), 0 /* Up */, pos(2, 1));
-    singleMoveTest("will move the rank down one square", pos(2, 1), 1 /* Down */, pos(1, 1));
-    singleMoveTest("will move the file left one square", pos(2, 2), 2 /* Left */, pos(2, 1));
-    singleMoveTest("will move the file right one square", pos(2, 2), 3 /* Right */, pos(2, 3));
-    singleMoveTest("will not move up due to out of bounds movement", pos(8, 4), 0 /* Up */, pos(8, 4));
-    singleMoveTest("will not move down due to out of bounds movement", pos(1, 4), 1 /* Down */, pos(1, 4));
-    singleMoveTest("will not move left due to out of bounds movement", pos(4, 1), 2 /* Left */, pos(4, 1));
-    singleMoveTest("will not move right due to out of bounds movement", pos(4, 8), 3 /* Right */, pos(4, 8));
+    singleMoveTest("will move the rank up one square", coord(1, 1), 0 /* Up */, coord(1, 2));
+    singleMoveTest("will move the rank down one square", coord(1, 2), 1 /* Down */, coord(1, 1));
+    singleMoveTest("will move the file left one square", coord(2, 2), 2 /* Left */, coord(1, 2));
+    singleMoveTest("will move the file right one square", coord(2, 2), 3 /* Right */, coord(3, 2));
+    singleMoveTest("will not move up due to out of bounds movement", coord(4, 8), 0 /* Up */, coord(4, 8));
+    singleMoveTest("will not move down due to out of bounds movement", coord(4, 1), 1 /* Down */, coord(4, 1));
+    singleMoveTest("will not move left due to out of bounds movement", coord(1, 4), 2 /* Left */, coord(1, 4));
+    singleMoveTest("will not move right due to out of bounds movement", coord(8, 4), 3 /* Right */, coord(8, 4));
 });
 describe("available move tests", function () {
-    pieceMoveTest("will find all available moves for the b2 pawn from the starting position", pos(2, 2), [pos(3, 2), pos(3, 1), pos(3, 3)]);
-    pieceMoveTest("will find all available moves for b1 knight from the starting position", pos(1, 2), [pos(3, 3), pos(3, 1)]);
+    pieceMoveTest("will find all available moves for the b2 pawn from the starting position", coord(2, 2), [coord(2, 3), coord(1, 3), coord(3, 3)]);
+    pieceMoveTest("will find all available moves for b1 knight from the starting position", coord(2, 1), [coord(3, 3), coord(1, 3)]);
     pieceMoveTest("will find all available moves for c1 bishop from the starting position", coord(3, 1), [coord(2, 2), coord(1, 3), coord(4, 2), coord(5, 3), coord(6, 4), coord(7, 5), coord(8, 6)]);
+    pieceMoveTest("will find all available moves for d1 queen from the starting position", coord(4, 1), []);
 });
 function singleMoveTest(message, start, direction, end) {
     it(message, function () {
@@ -36,11 +37,8 @@ function pieceMoveTest(message, start, expectedMoves) {
         //expectedMoves.forEach(move => expect(moves.some(m => m.rank === move.rank && m.file === move.file)).to.equal(true));
     });
 }
-function pos(rank, file) {
-    return { rank: rank, file: file };
-}
 function coord(file, rank) {
-    return pos(rank, file);
+    return { file: file, rank: rank };
 }
 function compare(left, right) {
     return left.rank === right.rank && left.file === right.file;
--- test/increments.ts
+++ test/increments.ts
@@ -11,20 +11,21 @@ console.log(classicBoard.toString());
 var expect = chai.expect;
 var dir = Chess.Direction;
 describe("single movement tests", () => {
-	singleMoveTest("will move the rank up one square", pos(1,1), dir.Up, pos(2,1));
-	singleMoveTest("will move the rank down one square", pos(2,1), dir.Down, pos(1,1));
-	singleMoveTest("will move the file left one square", pos(2,2), dir.Left, pos(2,1));
-	singleMoveTest("will move the file right one square", pos(2,2), dir.Right, pos(2,3));
-	singleMoveTest("will not move up due to out of bounds movement", pos(8,4), dir.Up, pos(8,4));
-	singleMoveTest("will not move down due to out of bounds movement", pos(1,4), dir.Down, pos(1,4));
-	singleMoveTest("will not move left due to out of bounds movement", pos(4,1), dir.Left, pos(4,1));
-	singleMoveTest("will not move right due to out of bounds movement", pos(4,8), dir.Right, pos(4,8));
+	singleMoveTest("will move the rank up one square", coord(1,1), dir.Up, coord(1,2));
+	singleMoveTest("will move the rank down one square", coord(1,2), dir.Down, coord(1,1));
+	singleMoveTest("will move the file left one square", coord(2,2), dir.Left, coord(1,2));
+	singleMoveTest("will move the file right one square", coord(2,2), dir.Right, coord(3,2));
+	singleMoveTest("will not move up due to out of bounds movement", coord(4,8), dir.Up, coord(4,8));
+	singleMoveTest("will not move down due to out of bounds movement", coord(4,1), dir.Down, coord(4,1));
+	singleMoveTest("will not move left due to out of bounds movement", coord(1,4), dir.Left, coord(1,4));
+	singleMoveTest("will not move right due to out of bounds movement", coord(8,4), dir.Right, coord(8,4));
 });
 
 describe("available move tests", () => {
-	pieceMoveTest("will find all available moves for the b2 pawn from the starting position", pos(2,2), [pos(3,2), pos(3,1), pos(3,3)]);
-	pieceMoveTest("will find all available moves for b1 knight from the starting position", pos(1,2), [pos(3,3), pos(3,1)]);
+	pieceMoveTest("will find all available moves for the b2 pawn from the starting position", coord(2,2), [coord(2,3), coord(1,3), coord(3,3)]);
+	pieceMoveTest("will find all available moves for b1 knight from the starting position", coord(2,1), [coord(3,3), coord(1,3)]);
 	pieceMoveTest("will find all available moves for c1 bishop from the starting position", coord(3,1), [coord(2,2), coord(1,3), coord(4,2), coord(5,3), coord(6,4), coord(7,5), coord(8,6)]);
+	pieceMoveTest("will find all available moves for d1 queen from the starting position", coord(4,1), []);
 });
 
 function singleMoveTest(message: string, start: Chess.Coordinate, direction: Chess.Direction, end: Chess.Coordinate): void {
@@ -44,12 +45,8 @@ function pieceMoveTest(message: string, start: Chess.Coordinate, expectedMoves: 
 	});
 }
 
-function pos(rank: number, file: number): Chess.Coordinate {
-	return { rank: rank, file: file };
-}
-
 function coord(file: number, rank: number) {
-	return pos(rank, file);
+	return { file: file, rank: rank };
 }
 
 function compare(left: Chess.Coordinate, right: Chess.Coordinate): boolean {
