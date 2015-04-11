0ee1c8a132b2b714f50d0eca1ddc53bd91c84472
Carl <carl@carlossus.com>
2015-04-11 08:51:34 +0800

    Moved 'getTransforms' to helper module

---
+++ src/engine/helpers/getTransforms.js
@@ -0,0 +1,39 @@
+var Chess = require("../../types");
+function getTransforms(singleMove, isWhite) {
+    // Return the inverse of the transform if from black perspective
+    var x = isWhite ? 1 : -1;
+    var up = { rank: 1 * x, file: 0 };
+    var down = { rank: -1 * x, file: 0 };
+    var left = { rank: 0, file: -1 * x };
+    var right = { rank: 0, file: 1 * x };
+    var upLeft = { rank: 1 * x, file: -1 * x };
+    var upRight = { rank: 1 * x, file: 1 * x };
+    var downLeft = { rank: -1 * x, file: -1 * x };
+    var downRight = { rank: -1 * x, file: 1 * x };
+    switch (singleMove.direction) {
+        case Chess.Direction.Up:
+            return [up];
+        case Chess.Direction.Down:
+            return [down];
+        case Chess.Direction.Left:
+            return [left];
+        case Chess.Direction.Right:
+            return [right];
+        case Chess.Direction.DiagonalUp:
+            return [upLeft, upRight];
+        case Chess.Direction.DiagonalDown:
+            return [downLeft, downRight];
+        case Chess.Direction.Diagonal:
+            return [upLeft, upRight, downLeft, downRight];
+        case Chess.Direction.Horizontal:
+            return [left, right];
+        case Chess.Direction.Vertical:
+            return [up, down];
+        case Chess.Direction.Lateral:
+            return [up, down, left, right];
+        default:
+            throw "InvalidDirectionException: The direction provided was invalid";
+    }
+}
+module.exports = getTransforms;
+//# sourceMappingURL=getTransforms.js.map
