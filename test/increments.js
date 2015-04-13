3a4992279b1eca1d0f7340fd513bf401086d591f
Carl <carl@carlossus.com>
2015-04-13 17:47:13 +0800

    Added a7 pawn test

--- test/increments.js
+++ test/increments.js
@@ -13,6 +13,7 @@ describe("available move tests", function () {
     pieceMoveTest("will find all available moves for e1 king from the starting position", coord(5, 1), [coord(4, 1), coord(6, 1), coord(4, 2), coord(5, 2), coord(6, 2)]);
     pieceMoveTest("will find all available moves for b7 pawn from the starting position", coord(2, 7), [coord(2, 6), coord(1, 6), coord(3, 6)]);
     pieceMoveTest("will find all available moves for b8 knight from the starting position", coord(2, 8), [coord(1, 6), coord(3, 6), coord(4, 7)]);
+    pieceMoveTest("will find all available moves for a7 pawn form the start position", coord(1, 7), [coord(1, 6), coord(2, 6)]);
 });
 function pieceMoveTest(message, start, expectedMoves) {
     it(message, function () {
