680a4b736a239a7e0f52b4c118c1400fcecdadb8
Carl <carl@carlossus.com>
2015-04-16 20:13:40 +0800

    Convert fen parser to a function

--- src/engine/parsers/fen.js
+++ src/engine/parsers/fen.js
@@ -1,58 +1,49 @@
 var fenStringParser = require("./stringParsers/fen");
-var FenParser = (function () {
-    function FenParser(parentEngine) {
-        this.defaultPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-        this.parentEngine = parentEngine;
-    }
-    FenParser.prototype.parse = function (position) {
-        //TODO Implement fen string parser, return a board
-        // Only accept 8x8 board?
-        var _this = this;
-        // This will split a FEN string into an array. First 8 indexes are ranks of the board, descending from rank 8 t rank 1.
-        this.engineInput = fenStringParser.parse(position);
-        // Fen strings start from the 8th rank, so we start from 8 and descend to rank 1.
-        var rankCount = this.parentEngine.rankCount;
-        this.engineInput.ranks.forEach(function (rank) {
-            _this.parentEngine.ranks[rankCount] = _this.createFilesForRank(rank, rankCount);
-            rankCount--;
-        });
+var defaultPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+function fenParser(position) {
+    var _this = this;
+    position = position || defaultPosition;
+    var engineInput = fenStringParser.parse(position);
+    var rankCount = this.rankCount;
+    engineInput.ranks.forEach(function (rank) {
+        _this.ranks[rankCount] = createFilesForRank(rank, rankCount);
+        rankCount--;
+    });
+}
+function createFilesForRank(fenRank, rankNumber) {
+    var rank = {
+        rank: rankNumber,
+        squares: []
     };
-    FenParser.prototype.createFilesForRank = function (fenRank, rankNumber) {
-        var rank = {
-            rank: rankNumber,
-            squares: []
-        };
-        for (var i = 1; i <= this.parentEngine.fileCount; i++) {
-            var notation = fenRank[i - 1];
-            var notationNumber = parseInt(notation);
-            // If the notation is a number, that many squares from this square contain no piece.
-            // TODO Consider refactoring--export to function for readability
-            if (!isNaN(notationNumber)) {
-                // Insert the next notation after the blank squares.
-                if (!!fenRank[i + 1])
-                    fenRank[i + notationNumber] = fenRank[i + 1];
-                // Insert blank squares from the current square, to currentSquare+notationNumber.
-                for (var j = i; j < i + notationNumber; j++)
-                    rank.squares.push({ file: j, piece: null });
-                i += notationNumber - 1;
-                continue;
-            }
-            var square = {
-                file: i,
-                piece: this.getPiece(notation)
-            };
-            square.piece.originalPosition = { rank: rank.rank, file: i };
-            rank.squares[i] = square;
+    for (var i = 1; i <= this.parentEngine.fileCount; i++) {
+        var notation = fenRank[i - 1];
+        var notationNumber = parseInt(notation);
+        // If the notation is a number, that many squares from this square contain no piece.
+        // TODO Consider refactoring--export to function for readability
+        if (!isNaN(notationNumber)) {
+            // Insert the next notation after the blank squares.
+            if (!!fenRank[i + 1])
+                fenRank[i + notationNumber] = fenRank[i + 1];
+            // Insert blank squares from the current square, to currentSquare+notationNumber.
+            for (var j = i; j < i + notationNumber; j++)
+                rank.squares.push({ file: j, piece: null });
+            i += notationNumber - 1;
+            continue;
         }
-        return rank;
-    };
-    FenParser.prototype.getPiece = function (notation) {
-        var pieceFactory = this.parentEngine.pieces.filter(function (p) { return p.notation.toUpperCase() === notation || p.notation.toLowerCase() === notation; });
-        return pieceFactory.length === 0
-            ? null
-            : pieceFactory[0].create(pieceFactory[0].notation.toUpperCase() === notation);
-    };
-    return FenParser;
-})();
-module.exports = FenParser;
+        var square = {
+            file: i,
+            piece: this.getPiece(notation)
+        };
+        square.piece.originalPosition = { rank: rank.rank, file: i };
+        rank.squares[i] = square;
+    }
+    return rank;
+}
+function getPiece(notation) {
+    var pieceFactory = this.pieces.filter(function (p) { return p.notation.toUpperCase() === notation || p.notation.toLowerCase() === notation; });
+    return pieceFactory.length === 0
+        ? null
+        : pieceFactory[0].create(pieceFactory[0].notation.toUpperCase() === notation);
+}
+module.exports = fenParser;
 //# sourceMappingURL=fen.js.map
