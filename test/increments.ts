f510d79e8239509d20beba26fda4b52b48b7d6df
Carl <carl@carlossus.com>
2015-05-02 22:43:30 +0800

    Movement tests updated to expect only valid moves

--- test/increments.ts
+++ test/increments.ts
@@ -10,20 +10,21 @@ console.log(classicEngine.toString());
 var expect = chai.expect;
 
 describe("available move tests", () => {
-	pieceMoveTest("will find all available moves for the b2 pawn from the starting position", coord(2,2), [coord(2,3), coord(1,3), coord(3,3)]);
+	pieceMoveTest("will find all available moves for the b2 pawn from the starting position", coord(2,2), [coord(2,3)]);
 	pieceMoveTest("will find all available moves for b1 knight from the starting position", coord(2,1), [coord(3,3), coord(1,3)]);
-	pieceMoveTest("will find all available moves for c1 bishop from the starting position", coord(3,1), [coord(2,2), coord(1,3), coord(4,2), coord(5,3), coord(6,4), coord(7,5), coord(8,6)]);
-	pieceMoveTest("will find all available moves for d1 queen from the starting position", coord(4,1), [coord(4,2), coord(4,3), coord(4,4), coord(4,5), coord(4,6), coord(4,7), coord(4,8)]);
-	pieceMoveTest("will find all available moves for e1 king from the starting position", coord(5,1), [coord(4,1), coord(6,1), coord(4,2), coord(5,2), coord(6,2)]);
-	pieceMoveTest("will find all available moves for b7 pawn from the starting position", coord(2,7), [coord(2,6), coord(1,6), coord(3,6)]);
-	pieceMoveTest("will find all available moves for b8 knight from the starting position", coord(2,8), [coord(1,6), coord(3,6), coord(4,7)]);
-	pieceMoveTest("will find all available moves for a7 pawn form the start position", coord(1,7), [coord(1,6), coord(2,6)]);
+	pieceMoveTest("will find all available moves for c1 bishop from the starting position", coord(3,1), []);
+	pieceMoveTest("will find all available moves for d1 queen from the starting position", coord(4,1), []);
+	pieceMoveTest("will find all available moves for e1 king from the starting position", coord(5,1), []);
+	pieceMoveTest("will find all available moves for b7 pawn from the starting position", coord(2,7), [coord(2,6)]);
+	pieceMoveTest("will find all available moves for b8 knight from the starting position", coord(2,8), [coord(1,6), coord(3,6)]);
+	pieceMoveTest("will find all available moves for a7 pawn form the start position", coord(1,7), [coord(1,6)]);
 });
 
 function pieceMoveTest(message: string, start: Chess.Coordinate, expectedMoves: Chess.Coordinate[]): void {
 	it(message, () => {
 		var moves = classicEngine.availableMoves(start);
 		expectedMoves.forEach(m => expect(moves).to.include({rank: m.rank, file: m.file }));
+		expect(expectedMoves.length).to.equal(moves.length);
 	});
 }
 
