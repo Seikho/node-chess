dce6df2be6cb48213dfce7b49e0fa788e45dffb7
Carl <carl@carlossus.com>
2015-05-17 22:06:32 +0800

    Added availableMoveTest for enpassant rule.

--- test/increments.ts
+++ test/increments.ts
@@ -31,6 +31,7 @@ describe("movement tests", () => {
 	pieceMoveTest("[Black] will move b7 pawn to b5", coord(2,7), coord(2,5), true);
 	pieceMoveTest("[White] will capture from a4 to b5", coord(1,4), coord(2,5), true);
 	pieceMoveTest("[Black] will move c7 pawn to c5, enabling enpassant capture", coord(3,7), coord(3,5), true);
+	pieceAvailableMovesTest("will find all available moves for white's b5 pawn", coord(2,5), [coord(2,6), coord(3,6)]);
 	pieceMoveTest("[White] will capture EnPassant from b5 to c6", coord(2,5), coord(3,6), true);
 });
 
