296155a343c6a366dc33f6d1373f5e15137b37d5
Carl <carl@carlossus.com>
2015-05-20 22:28:07 +0800

    Adding 'Direction' to exported API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 var Engine = require("./engine/engine");
 var classicEngine = require("./engine/instances/classic");
 var classicPieces = require("./engine/pieces/pieces");
+var Direction = require("./engine/direction");
 module.exports = {
     Engine: Engine,
     classicEngine: classicEngine,
     classicPieces: classicPieces,
+    Direction: Direction
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import Engine = require("./engine/engine");
 import classicEngine = require("./engine/instances/classic");
 import classicPieces = require("./engine/pieces/pieces");
+import Direction = require("./engine/direction");
 export = {
 	Engine: Engine,
 	classicEngine: classicEngine,
 	classicPieces: classicPieces,
+	Direction: Direction
 }
