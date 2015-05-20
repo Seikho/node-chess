ec19ad937f8308b612c6a026e5f0e2656615d0db
Carl <carl@carlossus.com>
2015-05-20 22:27:29 +0800

    Adding 'direction' to project. Resolving issue #29

---
+++ src/engine/direction.js
@@ -0,0 +1,21 @@
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+    Direction[Direction["DiagonalUp"] = 4] = "DiagonalUp";
+    Direction[Direction["DiagonalDown"] = 5] = "DiagonalDown";
+    Direction[Direction["Lateral"] = 6] = "Lateral";
+    Direction[Direction["Horizontal"] = 7] = "Horizontal";
+    Direction[Direction["Vertical"] = 8] = "Vertical";
+    Direction[Direction["Diagonal"] = 9] = "Diagonal";
+    Direction[Direction["UpLeft"] = 10] = "UpLeft";
+    Direction[Direction["UpRight"] = 11] = "UpRight";
+    Direction[Direction["DownLeft"] = 12] = "DownLeft";
+    Direction[Direction["DownRight"] = 13] = "DownRight";
+    Direction[Direction["KingSide"] = 14] = "KingSide";
+    Direction[Direction["QueenSide"] = 15] = "QueenSide";
+})(Direction || (Direction = {}));
+module.exports = Direction;
+//# sourceMappingURL=direction.js.map
---
+++ src/engine/direction.ts
@@ -0,0 +1,19 @@
+export = Direction;
+enum Direction {
+	Up,
+	Down,
+	Left,
+	Right,
+	DiagonalUp,
+	DiagonalDown,
+	Lateral,
+	Horizontal,
+	Vertical,
+	Diagonal,
+	UpLeft,
+	UpRight,
+	DownLeft,
+	DownRight,
+	KingSide,
+	QueenSide
+}
