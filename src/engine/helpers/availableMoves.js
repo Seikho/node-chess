ecea7c28d2fb837c2aa880e02c192c1eaa85a2dc
Carl <carl@carlossus.com>
2015-08-09 18:55:10 +0800

    Make availableMove helper module take and mutate boardState object

--- src/engine/helpers/availableMoves.js
+++ src/engine/helpers/availableMoves.js
@@ -1,6 +1,6 @@
-function availableMoves() {
+function availableMoves(boardState) {
     var self = this;
-    self.boardState.ranks.forEach(function (rank) {
+    boardState.ranks.forEach(function (rank) {
         rank.squares.forEach(function (square) {
             square.availableMoves = self.availableMoves({ file: square.file, rank: rank.rank });
         });
--- src/engine/helpers/availableMoves.ts
+++ src/engine/helpers/availableMoves.ts
@@ -1,10 +1,10 @@
 import Chess = require("node-chess");
 export = availableMoves;
 
-function availableMoves() {
+function availableMoves(boardState: Chess.BoardState) {
     var self: Chess.Engine = this;
     
-    self.boardState.ranks.forEach(rank => {
+    boardState.ranks.forEach(rank => {
         rank.squares.forEach(square => {
             square.availableMoves = self.availableMoves({ file: square.file, rank: rank.rank });
         });
