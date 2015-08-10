9f9862a6441f64a3e90f8efbb64918a219de8819
Carl Winkler <carl.winkler86@gmail.com>
2015-08-10 13:10:06 +0800

    Begin implementing check, checkmate and statelemtn 'rules' Required: board.allAvailableMoves(boardState) function will should be filterable by player

--- src/engine/instances/rules.js
+++ src/engine/instances/rules.js
@@ -1,9 +1,20 @@
 /**
  * If the board has the 'check' tag,
  */
-function isMoveAllowed(board) {
-    var isInCheck = board.boardState.tags["check"] === true;
+function isMoveAllowed(move, boardState, board) {
+    var isInCheck = isCheck(boardState);
     if (!isInCheck)
         return true;
+    var future = board.movePiece(move.from, move.to, boardState);
+    var futureIsInCheck = isCheck(future);
+    if (futureIsInCheck)
+        return false;
+}
+function isCheckmate(move, boardState, board) {
+    var isInCheck = isCheck(boardState);
+    // need all available moves module
+}
+function isCheck(boardState) {
+    return boardState.tags["check"] === true;
 }
 //# sourceMappingURL=rules.js.map
--- src/engine/instances/rules.ts
+++ src/engine/instances/rules.ts
@@ -2,9 +2,23 @@ import Chess = require("node-chess");
 /**
  * If the board has the 'check' tag,
  */
-function isMoveAllowed(board: Chess.Engine) {
-    var isInCheck = board.boardState.tags["check"] === true;
+function isMoveAllowed(move: Chess.Move, boardState: Chess.BoardState, board: Chess.Engine) {
+    var isInCheck = isCheck(boardState);
     if (!isInCheck) return true;
 
+    var future = board.movePiece(move.from, move.to, boardState);
+    var futureIsInCheck = isCheck(future);
     
+    if (futureIsInCheck) return false;
+    
+}
+
+function isCheckmate(move: Chess.Move, boardState: Chess.BoardState, board: Chess.Engine) {
+    var isInCheck = isCheck(boardState);
+    // need all available moves module
+    
+}
+
+function isCheck(boardState: Chess.BoardState) {
+    return boardState.tags["check"] === true;
 }
