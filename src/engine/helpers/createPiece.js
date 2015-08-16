348a647c66b819484e5edb4b8a300a939cd9c52a
Carl <carl@carlossus.com>
2015-08-16 22:02:49 +0800

    Add auto-incrementing ID to all pieces created using createPiece

--- src/engine/helpers/createPiece.js
+++ src/engine/helpers/createPiece.js
@@ -3,7 +3,11 @@ function createPiece(notation, location) {
     var matchingPiece = self.pieces.filter(function (p) { return p.notation === notation.toLocaleLowerCase(); });
     if (matchingPiece.length === 0)
         return null;
+    var count = self.boardState.tags["pieceCount"] || 0;
+    count++;
+    self.boardState.tags["pieceCount"] = count;
     var newPiece = new this.pieceFactory(matchingPiece[0], notation);
+    newPiece.id = count;
     newPiece.location = location;
     return newPiece;
 }
--- src/engine/helpers/createPiece.ts
+++ src/engine/helpers/createPiece.ts
@@ -3,12 +3,18 @@ export = createPiece;
 
 function createPiece(notation: string, location: Chess.Coordinate): Chess.BasePiece {
     var self: Chess.Engine = this;
-    
+
+
     var matchingPiece = self.pieces.filter(p => p.notation === notation.toLocaleLowerCase());
     if (matchingPiece.length === 0) return null;
-    
-    
+
+    var count = self.boardState.tags["pieceCount"] || 0;
+    count++;
+    self.boardState.tags["pieceCount"] = count;
+
     var newPiece = new this.pieceFactory(matchingPiece[0], notation);
+    newPiece.id = count;
+
     newPiece.location = location;
     return newPiece;
 }
