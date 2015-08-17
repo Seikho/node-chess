54282005707e198e6864b8a7e62847b1b007008b
Carl <carl@carlossus.com>
2015-08-17 18:40:16 +0800

    Remove tsd type def bundle

--- src/typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="./bluebird/bluebird.d.ts" />
-/// <reference path="./chai/chai.d.ts" />
-/// <reference path="./mocha/mocha.d.ts" />
-/// <reference path="./node/node.d.ts" />
-/// <reference path="./node-chess/node-chess.d.ts" />
