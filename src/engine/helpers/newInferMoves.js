2d3817859555b5f0f181d68e54f7be9670881b5a
Carl <carl@carlossus.com>
2015-08-24 22:39:07 +0800

    First implementation of newInferMoves -- need to integrate

--- src/engine/helpers/newInferMoves.js
+++ src/engine/helpers/newInferMoves.js
@@ -2,23 +2,79 @@ function infer(piece, boardState) {
     var self = this;
     boardState = boardState || self.boardState;
 }
-function process(move, piece, boardState) {
+function process(move, piece, boardState, board) {
     var modifier = piece.isWhite ? 1 : -1;
-    var transformedCoords = [];
+    var steps = [piece.location];
     var transforms = move.transforms;
-    if (transforms instanceof Array) {
-        for (var x = 0; x < transforms.length; x++) {
-            transformedCoords.push(applyTransform(piece.location, transforms[x], modifier));
-        }
+    if (!Array.isArray(transforms))
+        transforms = [transforms];
+    for (var x = 0; x < transforms.length; x++) {
+        var transform = transforms[x];
+        steps.push(applyTransform(steps[x], transform, modifier));
     }
-    else
-        transformedCoords.push(piece.location, transforms, modifier);
+    var finalCoord = steps[steps.length - 1];
+    var finalSquare = board.getSquare(finalCoord, boardState);
+    var finalSquarePiece = finalSquare.piece;
+    if (move.canCapture && finalSquarePiece.isWhite != piece.isWhite)
+        return null;
+    if (move.canMove && finalSquarePiece)
+        return null;
+    for (var x = 1; x < steps.length; x++) {
+        var prev = steps[x - 1];
+        var step = steps[x];
+        var transform = transforms[x - 1];
+        if (step !== finalCoord) {
+            //TODO: Allow 'squaresBetween' here			
+            if (transform.canJump)
+                continue;
+            var canMove = checkBetween(prev, step, piece, transform, boardState, board);
+            if (!canMove)
+                return null;
+            continue;
+        }
+        if (transform.canJump)
+            return [step];
+        // WIP: Need to check between squares again...
+        // For loop above needs to be converted to a function to be re-used for this section
+        var canMove = checkBetween(prev, step, piece, transform, boardState, board);
+        if (!canMove)
+            return null;
+        return [step];
+    }
+}
+// TODO: Shrink function signature. Take an object instead
+function checkBetween(start, end, piece, transform, boardState, board) {
+    var difference = {
+        file: Math.abs(start.file - end.file),
+        rank: Math.abs(start.rank - end.rank)
+    };
+    if (difference.file > 0 && difference.rank > 0)
+        throw new Error("Invalid non-jumpable move in " + piece.name + " definition: " + transform);
+    if (difference.file === 1 || difference.rank === 1)
+        return false;
+    var dimension = difference.file > 0 ? "file" : "rank";
+    var inc = end[dimension] > start[dimension] ? -1 : 1;
+    // Ensure all squares between current and previous are vacant
+    // Avoid closures to avoid heap allocations
+    for (var y = end[dimension]; y !== start[dimension]; y += inc) {
+        var between = { file: end.file, rank: end.rank };
+        between[dimension] += inc;
+        var sq = board.getSquare(between, boardState);
+        // If a square is occupied, the move is not valid
+        if (sq.piece)
+            return false;
+    }
+    // All squares are vacant
+    return true;
 }
 function applyTransform(coordinate, transform, modifier) {
+    if (transform.absolute)
+        modifier = 1;
     var file = coordinate.file + (transform.file * modifier);
     var rank = coordinate.rank + (transform.rank * modifier);
     return {
-        file: file, rank: rank
+        file: file,
+        rank: rank
     };
 }
 //# sourceMappingURL=newInferMoves.js.map
--- src/engine/helpers/newInferMoves.ts
+++ src/engine/helpers/newInferMoves.ts
@@ -6,25 +6,100 @@ function infer(piece: Chess.NewPiece, boardState?: Chess.BoardState) {
 
 }
 
-function process(move: Chess.MoveDefinition, piece: Chess.NewPiece, boardState: Chess.BoardState) {
+function process(move: Chess.MoveDefinition, piece: Chess.NewPiece, boardState: Chess.BoardState, board: Chess.Engine) {
 	var modifier = piece.isWhite ? 1 : -1;
-	var transformedCoords = [];
-	var transforms = move.transforms;
-	
-	if (transforms instanceof Array) {
-		for (var x = 0; x < transforms.length; x++) {
-			transformedCoords.push(applyTransform(piece.location, transforms[x], modifier));
+
+	var steps = [piece.location];
+	var transforms = <Chess.Transform[]>move.transforms;
+
+	if (!Array.isArray(transforms)) transforms = <any>[transforms];
+
+	for (var x = 0; x < transforms.length; x++) {
+		var transform = transforms[x];
+		steps.push(applyTransform(steps[x], transform, modifier));
+	}
+
+	var finalCoord = steps[steps.length - 1];
+	var finalSquare = board.getSquare(finalCoord, boardState);
+	var finalSquarePiece = finalSquare.piece;
+
+	if (move.canCapture && finalSquarePiece.isWhite != piece.isWhite) return null;
+	if (move.canMove && finalSquarePiece) return null;
+
+	for (var x = 1; x < steps.length; x++) {
+		var prev = steps[x - 1];
+		var step = steps[x];
+		var transform = transforms[x - 1];
+
+		if (step !== finalCoord) {
+			//TODO: Allow 'squaresBetween' here			
+			if (transform.canJump) continue;
+			var canMove = checkBetween(
+				prev,
+				step,
+				piece,
+				transform,
+				boardState,
+				board);
+
+			if (!canMove) return null;
+			continue;
 		}
-	} else transformedCoords.push(piece.location, transforms, modifier);
-	
-	
+
+		if (transform.canJump) return [step];
+		// WIP: Need to check between squares again...
+		// For loop above needs to be converted to a function to be re-used for this section
+		var canMove = checkBetween(
+			prev,
+			step,
+			piece,
+			transform,
+			boardState,
+			board);
+		
+		if (!canMove) return null;
+		return [step];
+	}
 }
 
-function applyTransform(coordinate: Chess.Coordinate, transform: Chess.Coordinate, modifier: number) {
+// TODO: Shrink function signature. Take an object instead
+function checkBetween(start: Chess.Coordinate, end: Chess.Coordinate, piece: Chess.NewPiece, transform: Chess.Transform, boardState: Chess.BoardState, board: Chess.Engine) {
+	var difference = {
+		file: Math.abs(start.file - end.file),
+		rank: Math.abs(start.rank - end.rank)
+	};
+
+	if (difference.file > 0 && difference.rank > 0)
+		throw new Error(`Invalid non-jumpable move in ${piece.name} definition: ${transform}`);
+
+	if (difference.file === 1 || difference.rank === 1) return false;
+
+	var dimension = difference.file > 0 ? "file" : "rank";
+	var inc = end[dimension] > start[dimension] ? -1 : 1;
+			
+	// Ensure all squares between current and previous are vacant
+	// Avoid closures to avoid heap allocations
+	for (var y = end[dimension]; y !== start[dimension]; y += inc) {
+		var between = { file: end.file, rank: end.rank };
+		between[dimension] += inc;
+		var sq = board.getSquare(between, boardState);
+				
+		// If a square is occupied, the move is not valid
+		if (sq.piece) return false;
+	}
+			
+	// All squares are vacant
+	return true;
+}
+
+function applyTransform(coordinate: Chess.Coordinate, transform: Chess.Transform, modifier: number) {
+	if (transform.absolute) modifier = 1;
+
 	var file = coordinate.file + (transform.file * modifier);
 	var rank = coordinate.rank + (transform.rank * modifier);
-	
+
 	return {
-		file, rank
+		file,
+		rank
 	};
 }
