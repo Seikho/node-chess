b86daa0d41fe388be635ee10a201949888c1fe1c
Carl <carl@carlossus.com>
2015-08-28 19:22:46 +0800

    Change base engine inferMoves to newInferMoves

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,6 +1,6 @@
 var toString = require("./helpers/toString");
 var getMoves = require("./helpers/getMoves");
-var inferMoves = require("./helpers/inferMoves");
+var inferMoves = require("./helpers/newInferMoves");
 var movePiece = require("./helpers/movePiece");
 var fenParser = require("./parsers/fen");
 var createSqaures = require("./helpers/createSquares");
