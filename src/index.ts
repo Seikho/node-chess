212b8e4906712e0a02c33bc5a45bb7a62758c985
Carl <carl@carlossus.com>
2016-04-28 18:34:25 +0800

    Export api as commonjs and es6 default

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import Engine = require("./engine/engine");
 import classicEngine = require("./engine/instances/classic/engine");
 import enums = require("./enums");
 import Directions = enums.Direction;
-export = chess;
 
-var chess = {
+const chess = {
 	Engine: Engine,
 	classic: {
 		engine: classicEngine,
 	},
 	Direction: enums.Direction
 };
+
+export = chess;
+exports.default = chess; 
+ 
