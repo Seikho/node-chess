e56b634c44ec339b1cd208fcc9e5d0984303b363
Sean Z <contact@seanzeng.com>
2020-04-30 14:59:27 +1200

    board to string

--- src/engine/helpers/toString.ts
+++ src/engine/helpers/toString.ts
@@ -1,9 +1,43 @@
 import Engine from '../index';
+import {BoardState} from "../../types";
+
+
+export function boardToString(board: BoardState): string {
+    let rankString: string[] = [];
+
+    // Generate Axis X String
+    let ranks: any = board.ranks[1]
+    const fileIndices = ranks.squares.length - 1;
+    let axisXNumbers = [...Array(fileIndices).keys()].map(i => `_${i+1}_`);
+    rankString.push(['-',...axisXNumbers].join('|'))
+
+    // Bottom to Top of Board
+    board.ranks.forEach((rank, i) => {
+        let s = '';
+
+        // Generate Axis Y String
+        s += `${i}|`
+
+        const pieces: string[] = []
+
+        // Left to right of board
+        rank.squares.forEach(sq => {
+            let p: string = sq.piece ? sq.piece.notation : '_'
+            pieces.push(`_${p}_`)
+        })
+
+        s += pieces.join('|')
+
+        rankString.push(s)
+    })
+
+    return rankString.reverse().join("\r\n");
+}
 
 export default function toString(this: Engine): string {
     var ranks: string[] = [];
     var fileLabels = ['-'];
-    for (var i = this.rankCount;i > 0; i--) {
+    for (var i = this.rankCount; i > 0; i--) {
         fileLabels[i] = "_" + i + "_";
         var pieces: any[] = [i];
         var rank = this.boardState.ranks[i];
